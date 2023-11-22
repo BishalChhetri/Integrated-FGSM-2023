dec4b3fa2e846bd53a94945f55f9c55f59abff08
BishalChhhetri <kcbisal1@gmail.com>
2023-11-22 12:24:21 +0545

    FGSM implemented

--- main.py
+++ main.py
@@ -1,125 +1,70 @@
-# class Employee:
-#     raise_amount = 1.04
-#
-#     def __init__(self,first_name,last_name,pay):
-#         self.first = first_name
-#         self.last= last_name
-#         self.pay = pay
-#         self.email = first_name + '.' + last_name + "@company.com"
-#
-#     def fullname(self):
-#         return '{} {}'.format(self.first, self.last)
-#
-#     def apply_raise(self):
-#         self.pay = int(self.pay * self.raise_amount)
-#
-# class Developer(Employee):
-#     raise_amount = 1.10
-#
-#     def __init__(self, first_name, last_name, pay, prog_lang):
-#         super().__init__(first_name,last_name,pay)
-#         self.prog_lang = prog_lang
-#
-#
-# class Manager(Employee):
-#
-#     def __init__(self,first_name, last_name, pay, employees=None):
-#         super().__init__(first_name, last_name, pay)
-#         if employees is None:
-#             self.employees = []
-#         else:
-#             self.employees = employees
-#
-#     def add_emp(self,emp):
-#         if emp not in self.employees:
-#             self.employees.append(emp)
-#
-#     def remove_emp(self,emp):
-#         if emp in self.employees:
-#             self.employees.remove(emp)
-#
-#     def print_emp(self):
-#         for emp in self.employees:
-#             print('---->',emp.fullname())
-#
-#
-# emply_1 = Developer('Bishal',"KC",25000, "Python")
-# emply_2 = Developer("Alice","Cordy",50000, "Java")
-#
-# mgr_1 = Manager("Sue","Smith", 90000,[emply_1])
-# print(mgr_1.email)
-# mgr_1.add_emp(emply_2)
-# mgr_1.remove_emp(emply_1)
-# mgr_1.print_emp()
-#
-# class Employee:
-#     raise_amount = 1.04
-#
-#     def __init__(self,first_name,last_name,pay):
-#         self.first = first_name
-#         self.last= last_name
-#         self.pay = pay
-#         self.email = first_name + '.' + last_name + "@company.com"
-#
-#     def fullname(self):
-#         return '{} {}'.format(self.first, self.last)
-#
-#     def apply_raise(self):
-#         self.pay = int(self.pay * self.raise_amount)
-#
-#     def __repr__(self):
-#         return "Employee('{}','{}',{})".format(self.first,self.last,self.pay)
-#
-#     def __str__(self):
-#         return '{} - {}'.format(self.fullname(),self.email)
-#
-# emp_1 = Employee("Bishal","KC",50000)
-# emp_2 = Employee("Sujin","Thapa",60000)
-#
-# print(repr(emp_1))
-# print(str(emp_1))
-#
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.models as models
+
+from utils import compute_gradient, read_image, to_array
+
+def func(inp, net=None, target=None):
+    out = net(inp)
+    loss = torch.nn.functional.nll_loss(out, target=torch.LongTensor([target]))
+
+    print(f"Loss: {loss.item()}")
+
+    return loss
+
+def attack(tensor, net, eps=1e-3, n_iter=50):
+    new_tensor = tensor.detach().clone()
+
+    orig_prediction = net(tensor).argmax()
+    print(f"Original Prediction: {orig_prediction.item()}")
+
+    for i in range(n_iter):
+        net.zero_grad()
+
+        grad = compute_gradient(
+            func, new_tensor, net=net, target = orig_prediction.item()
+        )
+        new_tensor = torch.clamp(new_tensor+eps*grad.sign(), -2,2)
+        new_prediction = net(new_tensor).argmax()
+
+        if orig_prediction != new_prediction:
+            print(f"We fooled the network after {i} iterations!")
+            print(f"New prediction: {new_prediction.item()}")
+            break
+
+    return new_tensor, orig_prediction.item(), new_prediction.item()
+
+if __name__ == "__main__":
+    net = models.resnet18(pretrained=True)
+    net.eval()
+
+    tensor = read_image("lion.jpeg")
+
+    new_tensor, orig_prediction, new_prediction = attack(
+        tensor, net, eps=1e-3, n_iter=100
+    )
+
+    _, (ax_orig, ax_new, ax_diff) = plt.subplots(1,3)
+    arr = to_array(tensor)
+    new_arr = to_array(new_tensor)
+    diff_arr = np.abs(arr - new_arr).mean(axis=-1)
+    diff_arr = diff_arr / diff_arr.max()
+
+    ax_orig.imshow(arr)
+    ax_new.imshow(new_arr)
+    ax_diff.imshow(diff_arr,cmap="gray")
+
+    ax_orig.axis("off")
+    ax_new.axis("off")
+    ax_diff.axis("off")
+
+    ax_orig.set_title(f"Original:{orig_prediction}")
+    ax_new.set_title(f"Modified: {new_prediction}")
+    ax_diff.set_title("Difference")
+
+    plt.savefig("response_1.png")
 
 
-# class Employee:
-#     raise_amount = 1.04
-#
-#     def __init__(self,first_name,last_name):
-#         self.first = first_name
-#         self.last= last_name
-#
-#     @property
-#     def email(self):
-#         return '{}.{}@email.com'.format(self.first, self.last)
-#     @property
-#     def fullname(self):
-#         return '{} {}'.format(self.first, self.last)
-#
-#     @fullname.setter
-#     def fullname(self,name):
-#         first,last = name.split(" ")
-#         self.first = first
-#         self.last = last
-#
-#     @fullname.deleter
-#     def fullname(self):
-#         print('Delete Name')
-#         self.first = None
-#         self.last = None
-#
-# emp_1 = Employee('John', 'Smith')
-#
-# emp_1.first = "Joe"
-# emp_1.fullname = "Corey Schafer"
-#
-# print(emp_1.first)
-# print(emp_1.email)
-# print(emp_1.fullname)
-#
-# del emp_1.fullname
 
-list = ["1", "4", "0", "6", "9"]
-list = [int(i) for i in list]
-list.sort()
-print (list)
 
---
+++ utils.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import torch
+from torchvision.transforms import (CenterCrop, Compose, Normalize, Resize, ToTensor)
+
+ # Compute Gradient
+def compute_gradient(func,inp, **kwargs):
+    inp.requires_grad = True
+
+    loss = func(inp, **kwargs)
+    loss.backward()
+
+    inp.requires_grad = False
+
+    return  inp.grad.data
+
+# Convert image into tensor image
+def read_image(path):
+    img = Image.open(path)
+
+    transform = Compose([Resize(256),
+                         CenterCrop(224),
+                         ToTensor(),
+                         Normalize(mean=[0.485,0.456,0.486],
+                                   std=[0.229,0.224,0.225])])
+
+    tensor_ = transform(img)
+    tensor = tensor_.unsqueeze(0)
+
+    return tensor
+
+def to_array(tensor):
+    tensor_ = tensor.squeeze()
+
+    unnormalize_transform = Compose([Normalize(mean=[0,0,0],
+                                               std=[1/0.229, 1/0.224, 1/0.225]),
+                                     Normalize(mean=[-0.485,-0.456,-0.406],
+                                               std=[1,1,1])])
+
+    arr_ = unnormalize_transform(tensor_)
+    arr = arr_.permute(1,2,0).detach().numpy()
+
+    return arr
+
+
+def scale_grad(grad):
+    grad_arr = torch.abs(grad).mean(dim=1).detach().permute(1,2,0)
+    grad_arr /= grad_arr.quantile(0.98)
+    grad_arr = torch.clamp(grad_arr,0,1)
+
+    return grad_arr.numpy()
